d095032044bd2c48c8e1746a82077d26bead9441
Lucas Petitjean <28061629+MushAsterion@users.noreply.github.com>
2025-11-30 00:39:17 +0000

    Removed Export Service

--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -1,5 +1,4 @@
 export 'application_providers.dart';
 export 'data_providers.dart';
 export 'error_provider.dart';
-export 'services_provider.dart';
 export 'ui_providers.dart';
--- lib/providers/services_provider.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:schemafx/services/export_service.dart';
-
-final exportServiceProvider = Provider((ref) => ExportService());
