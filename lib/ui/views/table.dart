d7026c45a59c66571bb6e39a8d5e9cb8f690b569
Lucas Petitjean <28061629+MushAsterion@users.noreply.github.com>
2025-12-03 22:47:38 +0000

    Removed extra import

--- lib/ui/views/table.dart
+++ lib/ui/views/table.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:schemafx/models/app_action.dart';
 import 'package:schemafx/models/models.dart';
 import 'package:schemafx/providers/providers.dart';
 
