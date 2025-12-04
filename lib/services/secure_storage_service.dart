4efcefda821def219e4ea1b282fb112bdc21f3b5
Lucas Petitjean <28061629+MushAsterion@users.noreply.github.com>
2025-12-04 22:15:20 +0000

    Simplified secure storage service

--- lib/services/secure_storage_service.dart
+++ lib/services/secure_storage_service.dart
@@ -4,15 +4,9 @@ class SecureStorageService {
   final _storage = const FlutterSecureStorage();
   static const _tokenKey = 'auth_token';
 
-  Future<void> saveToken(String token) async {
-    await _storage.write(key: _tokenKey, value: token);
-  }
+  Future<void> saveToken(String token) =>
+      _storage.write(key: _tokenKey, value: token);
 
-  Future<String?> getToken() async {
-    return await _storage.read(key: _tokenKey);
-  }
-
-  Future<void> deleteToken() async {
-    await _storage.delete(key: _tokenKey);
-  }
+  Future<String?> getToken() => _storage.read(key: _tokenKey);
+  Future<void> deleteToken() => _storage.delete(key: _tokenKey);
 }
